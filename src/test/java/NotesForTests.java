4e0ad15dcde8b38f8b5a22c4fe38d778e3c8459a
thescorchingsun <qbtestbq@gmai.com>
2022-09-07 23:21:17 +0200

    create test

---
+++ src/test/java/NotesForTests.java
@@ -0,0 +1,15 @@
+public class NotesForTests {
+
+    /*
+    driver.get(url); Открыть страницу сайта
+    driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")); Найти путь к элементу
+    menuBrowseLanguages.click(); Нажать на вкладку Browse Languages
+    Thread.sleep(1000); Задержка на 1 сек для удобства просмотра, при посте на GitHab лучше убрать
+    driver.quit(); Закрыть браузер после теста
+
+
+
+
+
+    */
+}
--- src/test/java/WebTestPractice.java
+++ src/test/java/WebTestPractice.java
@@ -2,12 +2,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static java.lang.Thread.*;
 
 public class WebTestPractice {
 
+    /*
+    Тест-кейс:
+    1.Открыть страницу https://www.99-bottles-of-beer.net/
+    2.Нажать пункт меню Browse Languages
+    3.Нажать на пункт меню Start
+    4.Подтвердить, что пользователь видит заголовок Welcome to 99 Bottles of Beer
+    5.Закрыть браузер
+     */
+
     @Test                                //ААА
-    public void testMenuStartTitle() {   //При переходе по ссылке открывается страница с меню старт
+    public void testMenuStartTitle() throws InterruptedException {   //При переходе по ссылке открывается страница с меню старт
 
         String chromeDriver = "webdriver.chrome.driver";
         String drivePath = "C:/Testing programs/chromedriver_win32/chromedriver.exe";
@@ -20,12 +32,24 @@ public class WebTestPractice {
         driver.get(url); //Открыть страницу сайта
 
         WebElement menuBrowseLanguages = driver.findElement( //Найти путь к элементу
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"));
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")
+        );
+        menuBrowseLanguages.click(); //Нажать на вкладку Browse Languages
+        //sleep(2000); //Задержка на 1 сек
 
-        menuBrowseLanguages.click(); //Нажать на пункт меню выше
+        WebElement menuStart = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
+        );
+        menuStart.click(); //Нажать на вкладку Start
+        //sleep(2000);
 
+        WebElement h2 = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h2")); //В элементе текст
+        String ActualResult = h2.getText(); //В переменной текст
+        //sleep(2000);
 
+        Assert.assertEquals(ActualResult, expectedResult);
 
+        driver.quit(); //Закрыть браузер после теста
 
     }
 }
